7cb7df872e5f6dc957ee4ed8c52c6a0ac06b1ddb
Arjo Chakravarty <arjo129@gmail.com>
2012-11-25 15:25:06 +0800

    Updated main matching function

--- phoneme.cpp
+++ phoneme.cpp
@@ -2,94 +2,41 @@
 
 char signal::getPhoneme(){
 	sample();
-	char t;
-	int pow = power();
-	int icomplexity = complexity(pow);
-	if(pow>SILENCE){
-		if(icomplexity>40){
-			//voice.voiceFormants();
-			mil++;
-			//Serial.println(icomplexity);
-			if(icomplexity>50){
-				if(icomplexity>60){
-					if(icomplexity>70){
-						if(icomplexity>80){
-							if(mil>5){
-								t = 's';
-							}
-							else{
-								t = 'k';
-							}
-						}
-						else{
-							t = 'z';
-						}
-					}
-					else{
-						if(mil>5){
-							t = 'z';
-						}
-						else{
-							t = 'g';
-						}
-					}
-				}
-				else{
-					if(mil>5){
-						t = 'v';
-					}
-					else{
-						t = 'p';
-					}
-				}
-			}
-			else{
-				if(mil>5){
-					t = 'v';
-				}
-				else{
-					t = 'p';
-				}
-			}
-			
+	if(power()>SILENCE){
+		coeff = complexity(power());
+		if(coeff<30 && coeff>20){
+			return 'u';
 		}
-		else{
-			//Perform Formant analysis
-			//Serial.println("vowel");
-			voiceFormants();
-			formantAnal();
-			if(formants[3]<8){
-				t = 'r';
-			}
-			if(formants[0]==1){
-				if(formants[1]>=9&&formants[1]<=11){
-					t = 'e';
-				}
-				else{
-					t = 'o';
-				}
+		else {
+			if(coeff<33&&coeff>25){
+				return 'e';
 			}
 			else{
-				if(formants[0] == 0){
-					if(formants[1]>=13 && formants[1]<=16){
-						t = 'i';
+				if(coeff<46&&coeff>33){
+					return 'o';
+				}
+				else{
+					if(coeff>50&&coeff<65){
+						return 'a';
 					}
 					else{
-						t = 'o';
+						if(coeff>70){
+							return 'h';
+						}
+						else{
+							if(coeff>90){
+								return 's';
+							}
+							return 'm';
+						}
 					}
 				}
-				else {
-					t = 'a';
-				}
 			}
-			mil =0;
-
 		}
 	}
 	else{
-		t = 'h';
+		return ' ';
 	}
-	return t;
 }
 void signal::formantAnal(){
 	int i = 0;

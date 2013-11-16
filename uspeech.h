97dedd780629d0512a7efb89dac15a5a7820a928
Arjo Chakravarty <arjo129@gmail.com>
2013-11-16 13:59:08 +0800

    Made codebender friendly

--- uspeech.h
+++ uspeech.h
@@ -20,6 +20,13 @@ public:
 	int arr[32],avgPower;
 	int calib;
     int testCoeff;
+    int minVolume;  /*!< This is the highest audio power that should be considered ready */
+    int fconstant;  /*!< This is the threshold for /f/, configure it yourself */
+    int econstant;  /*!< This is the threshold for /ee/, /i/, configure it yourself */
+    int aconstant;  /*!< This is the threshold for /a/ /o/ /r/ /l/, configure it yourself */
+    int vconstant;  /*!< This is the threshold for /z/ /v/ /w/, configure it yourself */
+    int shconstant; /*!< This is the threshold for /sh/ /ch/, above this everything else is regarded as /s/ */
+    BOOL f_enabled; /*!< Set this to false if you do not want to detect /f/s */
 	signal(int port);
     int micPower;
 	void sample();

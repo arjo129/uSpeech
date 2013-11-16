02cc917b0b72549d78075263d8846fd7db067f64
Arjo Chakravarty <arjo129@gmail.com>
2013-11-16 16:23:18 +0800

    more Codebender friendly

--- phoneme.cpp
+++ phoneme.cpp
@@ -1,15 +1,15 @@
 #include "uspeech.h"
 /**
-* The recognizer function
+* The recognizer function: takes 1-4ms to execute
 */
 char signal::getPhoneme(){
 	sample();
     unsigned int pp =power();
 	if(pp>SILENCE){
         
-		//Low pass filter for noise removal
+		//Perform Division
 		int k = complexity(pp);
-        
+        //Low pass filter for noise removal
 		overview[6] = overview[5];
 		overview[5] = overview[4];
 		overview[4] = overview[3];
@@ -33,44 +33,40 @@ char signal::getPhoneme(){
                 return 'f';
             }
         }
-        zeroCrossingSearch();
-    //Twiddle with the numbers here if your getting false triggers
-	//This is the main recognizer part
-	//Todo: use move values to header file
-		if(coeff<30 && coeff>20){
-			return 'u';
-		}
-		else {
-			if(coeff<33){
+
+	//Twiddle with the numbers here if your getting false triggers
+	//This is the main classifier part
+	
+        if(coeff<econstant /*Default value = 2*/){
 				return 'e';
 			}
 			else{
-				if(coeff<46){
+				if(coeff<aconstant /*Default value = 4*/){
 					return 'o';
 				}
 				else{
-					if(coeff<60){
+					if(coeff<vconstant /*Default value = 6*/){
 						return 'v';
 					}
 					else{
-						if(coeff<80){
+						if(coeff<shconstant /*Default value = 10*/){
 							return 'h';
 						}
 						else{
-							if(coeff>80){
+							if(coeff>shconstant){
 								return 's';
 							}
-							else{
-								return 'm';
-							}
+							
 						}
 					}
 				}
 			}
-		}
+		
 	}
 	else{
         micPower = 0;
+        vowelRatio = 0;
+        testCoeff = 0;
 		return ' ';
 	}
 }

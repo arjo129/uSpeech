fc785383f4dba49f10fc8b56e71bd1e3f3cf16e0
Arjo Chakravarty <arjo129@gmail.com>
2017-12-13 17:39:22 +0800

    Full pruning routine added

--- models/prunableGRU.py
+++ models/prunableGRU.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from dataset_generator import *
 from tensorflow.contrib.rnn import RNNCell
 from tensorflow.python.ops import variable_scope as vs
 from tensorflow.python.ops import math_ops
@@ -51,3 +52,40 @@ class PrunableGRU(RNNCell):
             hh = math_ops.tanh(math_ops.matmul(inputs,W_h)+math_ops.matmul(state*r,U_h)+self.b_h)
             h = (1-z)*hh + z*state
         return h,h
+
+class PrunableLogits:
+    def __init__(self,W,bias):
+        self.weights = tf.Variable(W,name="output_weight")
+        self.bias = tf.Variable(bias)
+        self.w_mask = tf.constant(get_mask(W))
+    def __call__(self,inputs):
+        return tf.matmul(inputs,self.weights*self.mask)+bias
+    
+def prune_model(model,batchsize = 50):
+    weights = model.get_weights()
+    W = weights[0]
+    U = weights[1]
+    bias = weights[2]
+    W_out = weights[3]
+    bias_out = weights[4]
+    GRU = PrunableGRU(W,U,bias)
+    Logits = PrunableLogits(self,W_out,bias_out)
+    state = tf.ones([batchsize, GRU.state_size])
+    X = tf.placeholder("float", [None, None, 2])
+    Y = tf.placeholder("float", [None, num_classes])
+    logits = Logits(GRU(X,state))
+    prediction = tf.nn.softmax(logits)
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+    optimizer = tf.train.GradientDescentOptimizer()
+    train_op = optimizer.minimize(loss_op)
+    correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    init = tf.global_variables_initializer()
+    dataset = build_dataset()
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(1000):
+            batch_x, batch_y = get_batch(dataset,batchsize=batchsize,batchtype="train")
+            sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+            loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,Y: batch_y})
+    

ddfcd4072377fb0ebdcc858347131988d290163a
Arjo Chakravarty <arjo129@gmail.com>
2017-12-22 04:17:05 +0800

    graph compiles now

--- models/prunableGRU.py
+++ models/prunableGRU.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from dataset_generator import *
-from tensorflow.contrib.rnn import RNNCell
+from tensorflow.contrib.rnn import RNNCell, static_rnn
 from tensorflow.python.ops import variable_scope as vs
 from tensorflow.python.ops import math_ops
 from tensorflow.python.framework.ops import convert_to_tensor
@@ -35,6 +35,8 @@ class PrunableGRU(RNNCell):
     @property
     def state_size(self):
         return self._num_units
+    def zero_state(self, batchsize, dtype):
+        return tf.ones([batchsize, self.state_size],dtype=dtype)
     def __call__(self,inputs, state, scope=None):
         inputs = convert_to_tensor(inputs)
         with vs.variable_scope(scope or type(self).__name__):
@@ -43,7 +45,7 @@ class PrunableGRU(RNNCell):
             W_h = tf.Variable(self.W_h,name="W_h")
             U_z = tf.Variable(self.U_z,name="U_z")
             U_r = tf.Variable(self.U_r,name="U_r")
-            U_z = tf.Variable(self.U_h,name="U_z")
+            U_h = tf.Variable(self.U_h,name="U_h")
             b_z = tf.Variable(self.b_z,name="b_z")
             b_r = tf.Variable(self.b_r,name="b_r")
             b_h = tf.Variable(self.b_h,name="b_h")
@@ -54,12 +56,12 @@ class PrunableGRU(RNNCell):
         return h,h
 
 class PrunableLogits:
-    def __init__(self,W,bias):
+    def __init__(self,W,bias,dtype="float32"):
         self.weights = tf.Variable(W,name="output_weight")
         self.bias = tf.Variable(bias)
-        self.w_mask = tf.constant(get_mask(W))
+        self.w_mask = tf.constant(get_mask(W),dtype=dtype)
     def __call__(self,inputs):
-        return tf.matmul(inputs,self.weights*self.mask)+bias
+        return tf.matmul(inputs,self.weights*self.w_mask)+self.bias
     
 def prune_model(model,batchsize = 50):
     weights = model.get_weights()
@@ -69,14 +71,15 @@ def prune_model(model,batchsize = 50):
     W_out = weights[3]
     bias_out = weights[4]
     GRU = PrunableGRU(W,U,bias)
-    Logits = PrunableLogits(self,W_out,bias_out)
-    state = tf.ones([batchsize, GRU.state_size])
-    X = tf.placeholder("float", [None, None, 2])
-    Y = tf.placeholder("float", [None, num_classes])
-    logits = Logits(GRU(X,state))
+    Logits = PrunableLogits(W_out,bias_out)
+    X = tf.placeholder("float", [None, batchsize, 2])
+    Y = tf.placeholder("float", [None, W_out.shape[1]])
+    X = tf.unstack(X,axis=1)
+    outputs, states = static_rnn(GRU, X, dtype=tf.float32)
+    logits = Logits(outputs[-1])
     prediction = tf.nn.softmax(logits)
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
-    optimizer = tf.train.GradientDescentOptimizer()
+    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
     train_op = optimizer.minimize(loss_op)
     correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
@@ -88,4 +91,10 @@ def prune_model(model,batchsize = 50):
             batch_x, batch_y = get_batch(dataset,batchsize=batchsize,batchtype="train")
             sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
             loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,Y: batch_y})
-    
+            print(loss)
+if __name__=="__main__":
+    from keras.models import load_model
+    import os
+    os.chdir("C:\\users\\arjo1\\documents\\data_google_speech")
+    model = load_model("gru40.hdf5")
+    prune_model(model)

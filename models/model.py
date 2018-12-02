ce2ce7ed01ce381f96f3f6e927804e1b9045cba4
Arjo Chakravarty <arjo129@gmail.com>
2018-12-02 11:08:06 +0800

    updated model so that it uses tensorflow only API instead of KERAS

--- models/model.py
+++ models/model.py
@@ -1,9 +1,9 @@
 from dataset_generator import *
 import tensorflow as tf
-from keras.models import Sequential,load_model
-from keras.layers import LSTM, Dropout, Dense, Bidirectional,GRU
-from keras.callbacks import TensorBoard,ModelCheckpoint
-from keras import optimizers
+from tensorflow.keras.models import Sequential, load_model, save_model
+from tensorflow.keras.layers import LSTM, Dropout, Dense, Bidirectional,GRU
+from tensorflow.keras.callbacks import TensorBoard,ModelCheckpoint
+from tensorflow.keras import optimizers
 
 def write_log(callback, names, logs, batch_no):
     for name, value in zip(names, logs):
@@ -14,25 +14,24 @@ def write_log(callback, names, logs, batch_no):
         callback.writer.add_summary(summary, batch_no)
         callback.writer.flush()
         
-def model():
-    adam = optimizers.adam(lr=0.05)
+def model(GRU_size=5):
+    adam = optimizers.Adam()
     model = Sequential()
-    model.add(Bidirectional(LSTM(20), input_shape=(None, 2)))
+    model.add(GRU(GRU_size))
     model.add(Dense(len(interested_words), activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer=adam)
     return model
 
-def train(model = model()):
+def train(model = model(),checkpoint = "latest.hdf5"):
     dataset = build_dataset()
     callback = TensorBoard("_tensor_board")
     callback.set_model(model)
     labels = interested_words
     for i in range(10000):
-        model.save("latest.hdf5")
+        save_model(model, "checkpoint.model")
         print("Chunk "+str(i)+" of 10000...")
-        X,Y = get_batch(dataset,batchsize=100)
+        X,Y = get_batch(dataset,batchsize=1000)
         for j in range(10):
-            #print(np.shape(X))
             logs = model.train_on_batch(np.array(X),np.array(Y))
             print("loss:",logs)
             write_log(callback, ["training loss"], [logs], i*10+j)
@@ -49,5 +48,7 @@ def train(model = model()):
                 pass
             if x == j: accuracy += 1
         write_log(callback,["test accuracy"],[accuracy],i)
-            
-train()
+
+if __name__ == "__main__":
+    os.chdir("C:\\users\\arjo1\\documents\\data_google_speech")
+    train()
